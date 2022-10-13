b4b7aec3a3978d4c9cdcad502675d67474bc0d45
rudugampola <ravindu365@gmail.com>
2022-10-13 08:11:36 -0700

    updated

--- commerce/settings.py
+++ commerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +131,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())

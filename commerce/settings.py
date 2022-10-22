3920824efbfe547c33c266cabf8e276e8b14eb65
rudugampola <ravindu365@gmail.com>
2022-10-22 09:26:11 -0700

    s3

--- commerce/settings.py
+++ commerce/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static'), )
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

be5f6a064f4320c8bd0a9f7987c9b05097790d64
rudugampola <ravindu365@gmail.com>
2022-10-22 09:45:02 -0700

    aws s3

--- auctions/models.py
+++ auctions/models.py
@@ -5,6 +5,8 @@ from django.dispatch import receiver
 from django.utils import timezone
 from PIL import Image
 
+from commerce.storage_backends import PrivateMediaStorage
+
 
 class User(AbstractUser):
     pass

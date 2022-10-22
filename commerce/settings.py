302d7c8e6df8ce53fa0571fded8337f5ecd358d7
rudugampola <ravindu365@gmail.com>
2022-10-22 09:23:33 -0700

    aws s3

--- commerce/settings.py
+++ commerce/settings.py
@@ -27,22 +27,6 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 IS_HEROKU = "DYNO" in os.environ
 
-# AWS Access Key S3
-AWS_ACCESS_KEY_ID = 'AKIAUMUGV5ILFY3HZSTI'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'bbay-auction'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'auctions/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'commerce.storage_backends.MediaStorage'
 
 # Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
 if IS_HEROKU:
@@ -162,8 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static'), )
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'auctions/static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -171,5 +155,22 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'auctions/templates'), )
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# AWS Access Key S3
+AWS_ACCESS_KEY_ID = 'AKIAUMUGV5ILFY3HZSTI'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'bbay-auction'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'auctions/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'auctions.storage_backends.MediaStorage'
 
 django_heroku.settings(locals())

6c12b566deb490b59ae418be56df7894ca2930a0
rudugampola <ravindu365@gmail.com>
2022-10-29 20:44:56 -0700

    Updated staticfiles

---
+++ staticfiles/auctions/js/main.js
@@ -0,0 +1,224 @@
+const spinnerBox = document.getElementById('spinner-box');
+const data = document.getElementById('data-box');
+
+// console.log(spinnerBox);
+// console.log(data);
+
+if (spinnerBox) {
+  $.ajax({
+    type: 'GET',
+    url: '/expenses',
+    success: function (response) {
+      setTimeout(() => {
+        spinnerBox.classList.add('not-visible');
+        data.classList.remove('not-visible');
+      }, 500);
+    },
+    error: function (error) {
+      setTimeout(() => {
+        dataBox.innerHTML = '<b>Failed to load data</b>';
+      }, 500);
+    },
+  });
+}
+
+var icon = document.getElementById('icon');
+icon.onclick = function () {
+  document.body.classList.toggle('darkmode');
+  if (document.body.classList.contains('darkmode')) {
+    // icon.src = "{% static 'img/sun.svg' %}";
+    icon.src = '/static/img/sun.svg';
+  } else {
+    // icon.src = "{% static 'img/moon.svg' %}";
+    icon.src = '/static/img/moon.svg';
+  }
+};
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+setTimeout(function () {
+  if ($('#msg').length > 0) {
+    $('#msg').fadeOut();
+  }
+}, 2000);
+
+// Only on the Login Page
+if (document.getElementById('login-form')) {
+  // Make submit button inactive until all fields are filled
+  $(document).ready(function () {
+    $('input[type="submit"]').prop('disabled', true);
+    $('input[type="text"]').keyup(function () {
+      if ($(this).val() != '' && $('input[type="password"]').val() != '') {
+        $('input[type="submit"]').prop('disabled', false);
+      }
+    });
+    $('input[type="password"]').keyup(function () {
+      if ($(this).val() != '' && $('input[type="text"]').val() != '') {
+        $('input[type="submit"]').prop('disabled', false);
+      }
+    });
+  });
+}
+
+document.onkeyup = function (e) {
+  if (e.ctrlKey && e.which == 67 && e.altKey) {
+    // Redirect to create page
+    window.location.href = '/create';
+  }
+};
+
+$(document).ready(function () {
+  $('input[id=passwordFirst]')
+    .keyup(function () {
+      var pswd = $(this).val();
+
+      //validate the length
+      if (pswd.length < 8) {
+        $('#length').removeClass('valid').addClass('invalid');
+      } else {
+        $('#length').removeClass('invalid').addClass('valid');
+      }
+
+      //validate letter
+      if (pswd.match(/[A-z]/)) {
+        $('#letter').removeClass('invalid').addClass('valid');
+      } else {
+        $('#letter').removeClass('valid').addClass('invalid');
+      }
+
+      //validate capital letter
+      if (pswd.match(/[A-Z]/)) {
+        $('#capital').removeClass('invalid').addClass('valid');
+      } else {
+        $('#capital').removeClass('valid').addClass('invalid');
+      }
+
+      //validate number
+      if (pswd.match(/\d/)) {
+        $('#number').removeClass('invalid').addClass('valid');
+      } else {
+        $('#number').removeClass('valid').addClass('invalid');
+      }
+
+      //validate space
+      if (pswd.match(/[^a-zA-Z0-9\-\/]/)) {
+        $('#space').removeClass('invalid').addClass('valid');
+      } else {
+        $('#space').removeClass('valid').addClass('invalid');
+      }
+    })
+    .focus(function () {
+      $('#pswd_info').show();
+    })
+    .blur(function () {
+      $('#pswd_info').hide();
+    });
+});
+
+// Get the Stars
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
+const fourth = document.getElementById('fourth');
+const fifth = document.getElementById('fifth');
+
+const form = document.querySelector('.rate-form');
+const confirmBox = document.getElementById('confirm-box');
+const csrf = document.getElementsByName('csrfmiddlewaretoken');
+
+const handleStarSelect = (size) => {
+  const children = form.children;
+  for (let i = 0; i < children.length; i++) {
+    if (i <= size) {
+      children[i].classList.add('checked');
+    } else {
+      children[i].classList.remove('checked');
+    }
+  }
+};
+
+const handleSelect = (selection) => {
+  switch (selection) {
+    case 'first':
+      handleStarSelect(1);
+      return;
+    case 'second':
+      handleStarSelect(2);
+      return;
+    case 'third':
+      handleStarSelect(3);
+      return;
+    case 'fourth':
+      handleStarSelect(4);
+      return;
+    case 'fifth':
+      handleStarSelect(5);
+      return;
+  }
+};
+
+const getNumericValue = (stringValue) => {
+  let numericValue;
+  if (stringValue === 'first') {
+    numericValue = 1;
+  } else if (stringValue === 'second') {
+    numericValue = 2;
+  } else if (stringValue === 'third') {
+    numericValue = 3;
+  } else if (stringValue === 'fourth') {
+    numericValue = 4;
+  } else if (stringValue === 'fifth') {
+    numericValue = 5;
+  }
+  return numericValue;
+};
+
+if (first) {
+  const arr = [first, second, third, fourth, fifth];
+
+  arr.forEach((item) => {
+    item.addEventListener('mouseover', (event) => {
+      handleSelect(event.target.id);
+    });
+  });
+
+  arr.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      const val = event.target.id;
+
+      let isSubmit = false;
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (isSubmit) {
+          return;
+        }
+        isSubmit = true;
+        const id = e.target.id;
+        console.log(id);
+        const val_num = getNumericValue(val);
+        console.log(val_num);
+
+        $.ajax({
+          type: 'POST',
+          url: '/rate_listing',
+          data: {
+            csrfmiddlewaretoken: csrf[0].value,
+            listing_id: id,
+            rating: val_num,
+          },
+          success: function (response) {
+            console.log(response);
+            // confirmBox.innerHTML = `<h1>Successfully rated with ${response.score}</h1>`;
+          },
+          error: function (error) {
+            console.log(error);
+            // confirmBox.innerHTML = `<h1>Failed to rate</h1>`;
+          },
+        });
+      });
+    });
+  });
+}
---
+++ staticfiles/mail/inbox.5fc4746dda8a.js
@@ -0,0 +1,226 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Use buttons to toggle between views
+  document
+    .querySelector('#inbox')
+    .addEventListener('click', () => load_mailbox('inbox'));
+  document
+    .querySelector('#sent')
+    .addEventListener('click', () => load_mailbox('sent'));
+  document
+    .querySelector('#archived')
+    .addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  // By default, load the inbox
+  localStorage.clear();
+  load_mailbox('inbox');
+
+  // Send email when compose form submitted
+  document.querySelector('form').onsubmit = send_email;
+});
+
+// compose_email(email.sender, email.recipients, email.subject, email.body);
+// "On Jan 1 2020, 12:00 AM foo@example.com wrote:"
+function compose_email(email, reply) {
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#single_email_view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  if (reply) {
+    document.querySelector('#compose-recipients').disabled = true;
+    document.querySelector('#compose-recipients').value = email.sender;
+    if (email.subject.substring(0, 3) === 'Re:')
+      document.querySelector('#compose-subject').value = email.subject;
+    else {
+      document.querySelector('#compose-subject').value = 'Re: ' + email.subject;
+    }
+    document.querySelector('#compose-body').innerHTML =
+      '\r\n' +
+      '\r\n' +
+      `>>>On ${email.timestamp} ${email.sender} wrote: ` +
+      '\r\n' +
+      '\r\n' +
+      email.body;
+  } else {
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+  }
+}
+
+function load_mailbox(mailbox) {
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single_email_view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${
+    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
+  }</h3>`;
+
+  // if (mailbox === 'sent') {
+  show_mail(mailbox);
+  // }
+}
+
+function send_email() {
+  recipients_compose = document.querySelector('#compose-recipients').value;
+  subject_compose = document.querySelector('#compose-subject').value;
+  body_compose = document.querySelector('#compose-body').value;
+
+  fetch('mail/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients_compose,
+      subject: subject_compose,
+      body: body_compose,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      // Print result
+      if (result.message === 'Email sent successfully.') {
+        localStorage.clear();
+        load_mailbox('sent');
+      } else {
+        const div = document.createElement('div');
+        div.className = 'alert alert-danger';
+        div.role = 'alert';
+        div.innerHTML =
+          'Error! Unable to send message to ' +
+          recipients_compose +
+          '. Email does not exist!';
+        document.querySelector('#compose-view').append(div);
+      }
+    });
+
+  return false;
+}
+
+function show_mail(mailbox) {
+  console.log('mail/emails/' + mailbox);
+
+  fetch('mail/emails/' + mailbox)
+    .then((response) => response.json())
+    .then((emails) => {
+      // Print emails
+      console.log(emails);
+
+      emails.forEach((email) => {
+        var prefix = mailbox === 'inbox' ? 'From: ' : 'To: ';
+        const div = document.createElement('div');
+        const subject = document.createElement('div');
+        const time = document.createElement('div');
+        const from_to = document.createElement('div');
+
+        if (mailbox === 'inbox') {
+          from_to.innerHTML = prefix + email.sender;
+        } else {
+          from_to.innerHTML = prefix + email.recipients;
+        }
+
+        subject.innerHTML = email.subject;
+        time.innerHTML = email.timestamp;
+        time.className = 'time';
+        subject.className = 'subject';
+
+        div.append(from_to);
+        div.append(subject);
+        div.append(time);
+        div.setAttribute('id', email.id);
+
+        if (email.read) {
+          div.className += 'email read';
+        } else {
+          div.className += 'email';
+        }
+
+        div.addEventListener('click', function () {
+          one_email(email.id);
+        });
+
+        document.querySelector('#emails-view').append(div);
+      });
+    });
+}
+
+function one_email(id) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single_email_view').style.display = 'block';
+
+  console.log('Email ID: ' + id + ' has been clicked.');
+
+  fetch(`mail/emails/${id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      update_read(id);
+      const subject = document.querySelector('#subject');
+      const from = document.querySelector('.from');
+      const to = document.querySelector('.to');
+      const body = document.querySelector('#body');
+      const timestamp = document.querySelector('#timestamp');
+      subject.innerHTML = email.subject;
+      from.innerHTML = email.sender;
+      to.innerHTML = email.recipients;
+      body.innerHTML = email.body;
+      timestamp.innerHTML = email.timestamp;
+
+      const reply = document.createElement('button');
+      reply.className = 'btn btn-success btn-sm right_align reply';
+      const reply_icon = document.createElement('i');
+      reply_icon.className = 'fa-solid fa-reply';
+      reply_icon.textContent = ' Reply';
+      reply.append(reply_icon);
+      from.append(reply);
+
+      const archive = document.createElement('button');
+      archive.className = 'btn btn-warning btn-sm right_align archive';
+      const archive_icon = document.createElement('i');
+      archive_icon.className = 'fa-solid fa-box-archive';
+      if (email.archived) {
+        archive_icon.textContent = ' Un-Archive';
+      } else {
+        archive_icon.textContent = ' Archive';
+      }
+      archive.append(archive_icon);
+
+      archive.addEventListener('click', function () {
+        if (email.archived) {
+          console.log(`updating email ${email.id} as archived = false`);
+          archive_mail(id, false);
+        } else {
+          console.log(`updating email ${email.id} as archived = true`);
+          archive_mail(id, true);
+        }
+      });
+      to.append(archive);
+
+      reply.addEventListener('click', function () {
+        compose_email(email, true);
+      });
+    });
+}
+
+function archive_mail(id, state) {
+  fetch(`mail/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: state,
+    }),
+  });
+  localStorage.clear();
+  load_mailbox('inbox');
+}
+
+function update_read(id) {
+  console.log(`updating email ${id} as read = true`);
+  fetch(`mail/emails/${id}`, {
+    method: 'PUT',
+    body: (body = JSON.stringify({
+      read: true,
+    })),
+  });
+}
